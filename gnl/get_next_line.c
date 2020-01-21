134db4c28c009ab8484522404a6f42c601e39863
Jonathan CAILLE <jocaille@e1r4p9.42.fr>
2020-01-21 15:37:59 +0100

    gnl

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -14,7 +14,88 @@
 
 int		get_next_line(int fd, char **line)
 {
-	static t_lstfd	*lst;
-	int				ret;
+	static char		*str = NULL;
 
+	if (!str)
+		str = ft_newstr(1);
+	if (!str || !line || BUFFER_SIZE < 1 || fd < 0)
+		return (-1);
+	if (ft_check_eol(str) >= 0)
+	{
+		str = ft_line(str, line, ft_check_eol(str));
+		return (1);
+	}
+	return (ft_read(fd, &str, line));
+}
+
+int		ft_read(int fd, char **str, char **line)
+{
+	char	buf[BUFFER_SIZE + 1];
+	int		ret;
+
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		*str = ft_join(*str, buf);
+		if (ft_check_eol(*str) >= 0)
+		{
+			*str = ft_line(*str, line, ft_check_eol(*str));
+			return (1);
+		}
+	}
+	if (ret < 0)
+		return (-1);
+	(void)ft_line(*str, line, -1);
+	return (0);
+}
+
+char	*ft_join(char *s, char *buf)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc(sizeof(char) * (ft_strlen(s) + ft_strlen(buf) + 1));
+	if (!str)
+		return (NULL);
+	str = ft_strcpy(str, s);
+	str = ft_strcat(str, buf);
+	free(s);
+	return (str);
+}
+
+int		ft_check_eol(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_line(char *s, char **line, int eol)
+{
+	char	*str;
+
+	if (eol >= 0)
+		*line = ft_substr(s, 0, eol);
+	if (eol >= 0 && eol < ft_strlen(s))
+		str = ft_substr(s, eol + 1, ft_strlen(s) - eol);
+	else if (eol >= 0 && eol == ft_strlen(s))
+		str = ft_newstr(1);
+	else
+	{
+		*line = ft_substr(s, 0, ft_strlen(s));
+		free(s);
+		return (NULL);
+	}
+	free(s);
+	if (!*line || !str)
+		return (NULL);
+	return (str);
 }
--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -12,16 +12,75 @@
 
 #include "get_next_line.h"
 
-void	ft_strncpy(char *buf, char *src, int size)
+int		ft_strlen(const char *str)
+{
+	int		len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strcpy(char *dst, const char *src)
 {
 	int		i;
 
 	i = 0;
-	while (i < size && src[i])
+	while (src[i])
 	{
-		buf[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
+	dst[i] = '\0';
+	return (dst);
 }
 
-t_lstfd	ft_newfd()
+char	*ft_strcat(char *s1, const char *s2)
+{
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(s1);
+	while (s2[i])
+	{
+		s1[len + i] = s2[i];
+		i++;
+	}
+	s1[len + i] = '\0';
+	return (s1);
+}
+
+char	*ft_newstr(size_t size)
+{
+	char	*str;
+
+	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	while (size + 1)
+		str[size--] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	s_len = ft_strlen(s);
+	while (i < len && start < s_len && s[start + i])
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
